54bdd06859af5ce203719ddcab69eb838ec366c4
Manuel Álvarez Díaz <malvarezdiaz@yahoo.es>
2022-08-03 12:30:17 +0200

    Eliminado modificador static de campos / métodos de clase Mock

--- rs-telco-model/src/main/java/es/udc/rs/telco/model/telcoservice/MockTelcoService.java
+++ rs-telco-model/src/main/java/es/udc/rs/telco/model/telcoservice/MockTelcoService.java
@@ -9,19 +9,19 @@ import es.udc.rs.telco.model.phonecall.PhoneCall;
 
 public class MockTelcoService implements TelcoService {
 
-	private static Map<Long, Customer> clientsMap = new LinkedHashMap<Long, Customer>();
-	private static Map<Long,PhoneCall> phoneCallsMap = new LinkedHashMap<Long,PhoneCall>();
-	private static Map<Long,List<PhoneCall>> phoneCallsByUserMap = new LinkedHashMap<Long,List<PhoneCall>>();
+	private Map<Long, Customer> clientsMap = new LinkedHashMap<Long, Customer>();
+	private Map<Long,PhoneCall> phoneCallsMap = new LinkedHashMap<Long,PhoneCall>();
+	private Map<Long,List<PhoneCall>> phoneCallsByUserMap = new LinkedHashMap<Long,List<PhoneCall>>();
 
-	private static long lastClientId = 0;
-	private static long lastPhoneCallId = 0;
+	private long lastClientId = 0;
+	private long lastPhoneCallId = 0;
 	
 
-	private static synchronized long getNextClientId() {
+	private synchronized long getNextClientId() {
 		return ++lastClientId;
 	}
 	
-	private static synchronized long getNextPhoneCallId() {
+	private synchronized long getNextPhoneCallId() {
 		return ++lastPhoneCallId;
 	}
 	
